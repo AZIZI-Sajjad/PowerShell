7dd49c2bf75031ffb09f55331f20d6d933e27d0d
AZIZI-Sajjad <sajjaad.azizi.021@gmail.com>
2024-11-17 21:47:00 +0100

    update 17-11-2024

---
+++ ping/ping.ps1
@@ -0,0 +1,26 @@
+﻿# *.csv file path all IP 
+$csvPath = "D:\CODE\PowerShell\ping\ip-list.csv"
+
+# Import *.csv file on all IP to ping
+$iplist = Import-CSV -Path $csvPath
+
+
+# function to PING each IP address
+function pingIP {
+    if ($(ping -n 10 -w 5 $ip > $null; $?)){
+    Write-Host "$ip"
+    # Write-Host "$ip : up"
+    }
+
+    <#
+    else {
+    Write-Host "$ip : down"
+    }
+    #>
+
+    }
+
+foreach ($ip in $iplist.device) {
+    # Write-Host "IP: $ip"
+    pingIP
+}
